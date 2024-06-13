43f5a71f369cf9290460a3a2af66e37d55c3976b
Aydar Farrakhov <stranniknm@gmail.com>
2024-06-13 14:41:09 +0300

    add rest api

--- api/src/main.ts
+++ api/src/main.ts
@@ -17,6 +17,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
   initSwagger(app);
-  await app.listen(3000);
+  await app.listen(4000);
 }
 bootstrap();
